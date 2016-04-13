05b8d14bafaeeb834254940426dd4797c5392f19
stasa <magarence@gmail.com>
2016-04-13 02:00:02 +0200

    dodat php

---
+++ test.php
@@ -0,0 +1,103 @@
+<?
+
+/**
+ * @author stasa <stasas@gmail.com>
+ * @version 3.3 nova
+ *
+ * 
+ */
+/*
+
+ zmena2
+
+jos izmena
+izbacene srednje izmene.
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+
+izmene vezane za verziju 2
+
+jos izmena za ver2 --blabla23423fdsva JOS IZMENA ZA VER2 --BLABLA bugfix za master branch
+izmena vani 
+
+<?php
+
+*/
+ddsdf ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		foreach ($variable as $key => $value) {
+			# code...
+			try {
+				mysql_connect()
+			} catch (Exception $e) {
+				
+			}
+		}
+	}
+}
+ddsdf ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		foreach ($variable as $key => $value) {
+			# code...
+			try {
+				mysql_connect()
+			} catch (Exception $e) {
+				
+			}
+		}
+	}
+}
+
+ddsdf ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+
+foreach ($variable as $key => $value) {
+	# code...
+}
+
+if (condition) {
+	# code...
+} else {
+	# code...
+}
+	/**
+	 * [FunctionName description]
+	 * @param string $value [description]
+	 */
+	public function FunctionName($value='') {<h1></h1> } ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+?>
